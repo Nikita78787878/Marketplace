51b555fa9deb87aeac774b55149f501cebad36b4
Epov Nikita <Nikita.Epov@develonica.ru>
2026-07-20 20:12:03 +0800

    доработка маппера

---
+++ src/main/java/org/example/marketplace/dto/CreateProductRequest.java
@@ -0,0 +1,27 @@
+package org.example.marketplace.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+
+import java.math.BigDecimal;
+
+public record CreateProductRequest(
+        @NotBlank
+        String name,
+
+        @NotBlank
+        String description,
+
+        @Positive
+        @NotNull
+        BigDecimal price,
+
+        @PositiveOrZero
+        @NotNull
+        Integer stockQuantity,
+
+        @NotNull
+        Long categoryId) {
+}
